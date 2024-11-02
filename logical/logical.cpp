a27065238be00dc9d2d220d224e3ab494d0c0225
HP <HP@DESKTOP-NMBILJD>
2024-11-02 20:11:42 +0100

    Add project files.

---
+++ logical/logical.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+
+	int A = 1, B = 0;
+	 
+	cout << (A && B) << endl;
+	cout << (A || B) << endl;
+	cout << !A << endl;
+	cout << !B << endl;
+	cout << !(A && B) << endl;
+	cout << !(A || B) << endl;
+	  
+
+
+	return 0;
+
+}
